e004cd5735fc1ac55431c80e4617de12e280308e
joseunghyo <jhoseunghyo7742@likelion.org>
2021-03-01 00:11:48 +0900

    로그인,회원가입 구현

--- secondproject/secondproject/urls.py
+++ secondproject/secondproject/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blog2.views.home,name="home"),
     path('blog/',include('blog2.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
